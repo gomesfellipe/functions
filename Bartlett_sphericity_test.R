8ed5310d6408d003fc43b99157bcaa8f062bc088
Fellipe <gomes.fellipe@hotmail.com>
2018-01-04 20:16:41 -0200

    fix

--- Bartlett_sphericity_test.R
+++ Bartlett_sphericity_test.R
@@ -1,10 +1,12 @@
-# The Bartlett's test statistic indicates to what extent we deviate from the reference situation |R| = 1.
+#Teste de Bartlett - a hipótese nula da matriz de correlação ser uma matriz identidade ( $| R | = 1$ ), 
+#isto é, avalia se os componentes fora da diagonal principal são zero. 
+#O resultado significativo indica que existem algumas relações entre as variáveis.
 
 Bartlett.sphericity.test <- function(x)
 {
-  method <- "Bartlett's test of sphericity"
+  method <- "Teste de esfericidade de Bartlett"
   data.name <- deparse(substitute(x))
-  x <- subset(x, complete.cases(x)) # Omit missing values
+  x <- subset(x, complete.cases(x)) # Omitindo valores faltantes
   n <- nrow(x)
   p <- ncol(x)
   chisq <- (1-n+(2*p+5)/6)*log(det(cor(x)))
@@ -15,3 +17,4 @@ Bartlett.sphericity.test <- function(x)
   return(structure(list(statistic=chisq, parameter=df, p.value=p.value,
                         method=method, data.name=data.name), class="htest"))
 }
+Bartlett.sphericity.test(dados)
