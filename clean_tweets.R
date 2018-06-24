198e9f8f1a84ff8bee9f66d222e2e9cff09f96f0
Fellipe <gomes.fellipe@hotmail.com>
2018-06-24 14:10:45 -0300

    Update clean_tweets.R

--- clean_tweets.R
+++ clean_tweets.R
@@ -3,18 +3,6 @@
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 clean_tweets<- function(tweet){
 library(stringr)
-  # Captação de erros de codificacao:
-catch.error = function(x){
-  # let us create a missing value for test purpose
-  y = NA
-  # Try to catch that error (NA) we just created
-  catch_error = tryCatch(tolower(x), error=function(e) e)
-  # if not an error
-  if (!inherits(catch_error, "error"))
-    y = tolower(x)
-  # check result if error exists, otherwise the function works fine.
-  return(y)
-}
   
   # Limpe o tweet para análise de sentimentos
   
@@ -39,7 +27,7 @@ catch.error = function(x){
 
   
   #Converte tudo para minusculo 
-  tweet = catch_error(tweet)                                    # Aplica a funcao catch.error 
+  tweet = tolower(tweet)
   
   return(tweet)
 }
