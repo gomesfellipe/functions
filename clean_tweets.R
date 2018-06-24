1c0f35f6b0510170db07e480111dcf8c7c097dbd
Fellipe <gomes.fellipe@hotmail.com>
2018-06-24 14:14:12 -0300

    Create clean_tweets.R

---
+++ clean_tweets.R
@@ -0,0 +1,36 @@
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#+                            Limpeza de caracteres especiais                                   +
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+clean_tweets<- function(tweet){
+  
+library(stringr)
+  
+  # Limpe o tweet para análise de sentimentos
+  
+  tweet = gsub("(f|ht)(tp)(s?)(://)(.*)[.|/](.*)", " ", tweet)  # Remove html links
+  tweet = gsub("(RT|via)((?:\\b\\W*@\\w+)+)", " ", tweet)       # Remove retweet 
+  tweet = gsub("#\\w+", " ", tweet)                             # Remove todos "#Hashtag"
+  tweet = gsub("@\\w+", " ", tweet)                             # Remove todos "@people"
+  tweet = gsub("[[:punct:]]", " ", tweet)                       # Remove todas as pontuacoes
+  tweet = gsub("[[:digit:]]", " ", tweet)                       # Remover numeros, precisamos apenas de texto para análise
+  
+  tweet = gsub("[ \t]{2,}", " ", tweet)                         # Remove espaços desnecessarios
+  tweet = gsub("^\\s+|\\s+$", "", tweet)                        # (espacos em branco, tabs etc)
+  
+  tweet = gsub('https://','',tweet)                             # Remove https://
+  tweet = gsub('http://','',tweet)                              # Remove http://
+  tweet = gsub('[^[:graph:]]', ' ',tweet)                       # Remove strings gráficos como emoticons
+  tweet = gsub('[[:punct:]]', '', tweet)                        # Remove pontuacao 
+  tweet = gsub('[[:cntrl:]]', '', tweet)                        # Remove strings de controle
+  tweet = gsub('\\d+', '', tweet)                               # Remove numeros
+  tweet=str_replace_all(tweet,"[^[:graph:]]", " ")              # Remove strings gráficos como emoticons
+  #tweet=SnowballC::wordStem(tweet,language = lang)     # Aplica steamming (desativado) 
+
+  
+  #Converte tudo para minusculo 
+  tweet = tolower(tweet)
+  
+  return(tweet)
+}
+#Referencia: https://sites.google.com/site/miningtwitter/questions/talking-about/wordclouds/comparison-cloud
+#-----------------------------------------------------------------------------------------------
