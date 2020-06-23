e51ed591134a4e156f26c4b8fe8bf3709eac0ebb
Fellipe <gomes.fellipe@hotmail.com>
2020-06-23 19:27:10 -0300

    Create detect_pkg_version.R

---
+++ detect_pkg_version.R
@@ -0,0 +1,31 @@
+# Detectar a versão a e origem de um ou mais pacotes
+
+detect_pkg_version <- detect_function(package){
+  
+  if(length(package) == 1){
+    desc <- packageDescription(package)  
+    
+    if(is.null(desc$RemoteType)){
+      glue::glue("{desc$Package} {desc$Version}")  
+    }else if(desc$RemoteType == "github"){
+      glue::glue("{desc$Package} {desc$Version} ({desc$GithubUsername}/{desc$GithubRepo})")
+    }
+    
+  }else if(length(package) > 1){
+    desc <- lapply(package, packageDescription)
+    
+    desc <- 
+    unlist(
+      lapply(desc, function(desc){
+        if(is.null(desc$RemoteType)){
+          glue::glue("{desc$Package} {desc$Version}")  
+        }else if(desc$RemoteType == "github"){
+          glue::glue("{desc$Package} {desc$Version} ({desc$GithubUsername}/{desc$GithubRepo})")
+        }  
+      })  
+    )
+    tibble::tibble(package = desc)
+  }
+  
+  
+}
